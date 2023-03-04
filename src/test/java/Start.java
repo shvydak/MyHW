5ce4ce097cdbfbe7e7157653daa680d2d07c90bb
shvydak <y.shvydak@gmail.com>
2023-03-04 18:04:51 +0200

    Update Start.java

--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -1,11 +1,16 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Start {
 WebDriver wd;
 
 @Test
-    public void start
+    public void start(){
+    wd = new ChromeDriver();
+    wd.get("http://www.google.com");
+    wd.quit();
+}
 
 
 
